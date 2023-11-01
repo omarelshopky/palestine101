10c751cfbcbcc1c50f6ca1f37f016b63931bd314
Omar Elshopky <omar.elshopky202@gmail.com>
2023-11-01 17:32:25 +0200

    Hero section circles animation

--- src/components/HeroSection/HeroSection.vue
+++ src/components/HeroSection/HeroSection.vue
@@ -9,11 +9,13 @@ const unusedVariableToEnableMixin = false;
             <div :class="bem({ element: 'LeftColumn' })">
                 <span :class="bem({ element: 'Border', mod: { type: 'vertical', location: 'leftRight' } })"></span>
                 <span :class="bem({ element: 'Border', mod: { type: 'vertical', location: 'leftLeft' } })"></span>
+                <span :class="bem({ element: 'Circle', mod: { color: 'green', location: 'topLeft' } })"></span>
             </div>
             <div :class="bem({ element: 'RightColumn' })">
                 <span :class="bem({ element: 'Border', mod: { type: 'vertical', location: 'rightRight' } })"></span>
                 <span :class="bem({ element: 'Border', mod: { type: 'vertical', location: 'rightLeft' } })"></span>
                 <span :class="bem({ element: 'RightColumnFarLeftBorder' })"></span>
+                <span :class="bem({ element: 'Circle', mod: { color: 'green', location: 'bottomRight' } })"></span>
             </div>
             <div :class="bem({ element: 'UpperRow' })">
                 <span :class="bem({ element: 'Border', mod: { type: 'horizontal', location: 'lower' } })"></span>
@@ -22,6 +24,8 @@ const unusedVariableToEnableMixin = false;
                 <span :class="bem({ element: 'Border', mod: { type: 'horizontal', location: 'upper' } })"></span>
             </div>
             <div :class="bem({ element: 'CenterWrapper' })">
+                <span :class="bem({ element: 'Circle', mod: { color: 'red', location: 'bottomLeft' } })"></span>
+                <span :class="bem({ element: 'Circle', mod: { color: 'red', location: 'topRight' } })"></span>
                 CenterWrapper
             </div>
         </div>
