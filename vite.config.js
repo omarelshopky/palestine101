5ed400afa5947fb719dcb940c77a5d9bf78d03d7
Omar Elshopky <omar.elshopky202@gmail.com>
2024-06-16 16:52:38 +0300

    Prepare for GH pages deployment

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/palestine101/',
   plugins: [
     vue(),
     VueI18nPlugin({
